a63d83296d839452e8fcc3f97ea7dd50b9be2997
diazAaron <dbaaron@live.com>
2023-01-09 12:06:04 +0100

    Agregar archivos de proyecto.

---
+++ SuperheroesMVVM/ServicioSuperheroes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroesMVVM
+{
+    class ServicioSuperheroes
+    {
+        public ObservableCollection<Superheroe> GetSuperheroes() {
+            return Superheroe.GetSamples();
+        }
+    }
+}
